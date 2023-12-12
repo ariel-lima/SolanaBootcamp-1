8ded118e35148690dd62dc1a89d3b7c2e0b40392
ariel lima <ariel_lima_oliveira@yahoo.com.br>
2023-12-12 16:03:07 +0000

    homerwork 5

--- homeworks_rust/homeworks/homework5/functions/functions2.rs
+++ homeworks_rust/homeworks/homework5/functions/functions2.rs
@@ -1,14 +1,15 @@
 // functions2.rs
 // Make me compile! Execute `rustlings hint functions2` for hints :)
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_this(3);
+  let num = 3;
+  call_this(num);
 }
 
-fn call_this(num:) {
-    for i in 0..num {
-        println!("Loop! number {}", i + 1);
-    }
+fn call_this(num: u32) {
+  for i in 0..num {
+      println!("Loop! number {}", i + 1);
+  }
 }
